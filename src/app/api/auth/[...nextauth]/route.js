a4b23d49876143d45b5ceec19443ec56d4b2886a
alialtunar <altunarali284@gmail.com>
2024-06-11 19:52:03 +0300

    add next-auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,16 @@
+import nextAuth from "next-auth";
+import NextAuth from "next-auth/next";
+
+import GoogleProvider from 'next-auth/providers/google';
+
+
+const handler = nextAuth({
+    providers:[
+            GoogleProvider({
+                clientId: process.env.GOOGLE_ID,
+                clientSecret: process.env.GOOGLE_SECRET,
+            }),
+    ],
+});
+
+export { handler as GET, handler as POST };
