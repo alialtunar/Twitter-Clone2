54228c8c1f8f27ed7363f6347195348fae83b763
alialtunar <altunarali284@gmail.com>
2024-06-11 19:32:04 +0300

    update news component

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,12 +14,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div className="flex justify-between max-w-6xl mx-auto">
-        <div>
+      <div className="flex justify-between max-w-7xl mx-auto">
+        <div className="hidden  sm:inline border-r h-screen">
           <Sidebar/>
         </div>
        <div> {children}</div>
-       <div><News/></div>
+       <div className="lg:flex-col p-3 h-screen border-l  hidden lg:flex w-[24rem]">
+        <div className="sticky top-0 bg-white py-2">
+        <input type="text" placeholder="Search" className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"></input>
+        </div>
+        <News/>
+        </div>
       </div>
       
         </body>
